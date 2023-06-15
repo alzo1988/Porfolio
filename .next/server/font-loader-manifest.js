8fca9bfebda64af3f08cb8cf9b78dac91db7a1ab
alzo1988 <56118360+alzo1988@users.noreply.github.com>
2023-06-15 04:11:17 -0400

    .next
    Commit du dossier .next

---
+++ .next/server/font-loader-manifest.js
@@ -0,0 +1,9 @@
+self.__FONT_LOADER_MANIFEST={
+  "pages": {
+    "/_app": [
+      "static/media/eb15695615c924c8.p.woff2",
+      "static/media/1060bab20f18b5c2.p.woff2"
+    ]
+  },
+  "app": {}
+}
