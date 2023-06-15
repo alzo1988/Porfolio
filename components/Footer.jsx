a7dfe6237bd19c30d6dc1fe01bf463e31ed2553b
alzo1988 <56118360+alzo1988@users.noreply.github.com>
2023-06-15 04:13:52 -0400

    les composantes
    Commit des differents composants

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Footer.module.css'
+
+const Footer = () => {
+    return <div className={styles.footer}>
+        <p>&copy; Alle Dieng - Développeur web Frontend</p>
+    </div>
+
+};
+
+export default Footer;
